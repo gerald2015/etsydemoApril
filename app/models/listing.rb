dfff7fed595e4015f84fa72cc4f5f3d72056c921
gerald2015 <gjackson.hu@gmail.com>
2015-04-19 12:23:04 +0000

    added configure_permitted_parameters to app_controller for devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,6 @@ class Listing < ActiveRecord::Base
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   belongs_to :user
-  has_many :orders
-
+  has_many :orders, dependent: :destroy
+ 
 end
